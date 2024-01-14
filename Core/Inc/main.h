c368674cd01f6f0fbec20e07e6fc00c0f0dfd921
sarangaSena <saranganirwana@gmail.com>
2024-01-14 11:36:34 +0530

    Update All

---
+++ Core/Inc/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Dec 9, 2023
+ *      Author: Saranga
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "py32f0xx_hal.h"
+
+
+#endif /* INC_MAIN_H_ */
+
